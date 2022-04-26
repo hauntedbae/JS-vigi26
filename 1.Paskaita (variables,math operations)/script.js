4324872f218aeec054d03aa7b97c363d9efdc2c0
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-04-26 20:11:25 +0300

    Just started :)

---
+++ 1.Paskaita (variables,math operations)/script.js
@@ -0,0 +1,14 @@
+// One line comment 
+
+/*
+    Sveikia cia yra
+    Multi line comment
+*/
+
+//  Sukuriam kintamuosius (inicializuojam kintamaji)
+let myName;
+let age;
+
+//  Priskiriam kintamiejam reiksmes
+myName = "Rima";
+age = 26;
