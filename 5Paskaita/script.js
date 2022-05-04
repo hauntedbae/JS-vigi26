a4994a1471ab0071228ada6715bf16dde8343d19
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-05-04 20:07:42 +0300

    Lecture about DOM, querySelector, DOM navigations

---
+++ 5Paskaita/script.js
@@ -0,0 +1,33 @@
+const nameNode = document.getElementById("name");
+nameNode.textContent = "Tomas";
+
+const descriptionNode = document.getElementById("description");
+descriptionNode.textContent = "Man patinka programuoti.";
+
+document.body.style.backgroundColor = "#f1f1f1";
+document.body.style.margin = 0;
+nameNode.style.color = "blue";
+nameNode.style.paddingleft = "10px";
+descriptionNode.style.padding = "10px";
+descriptionNode.style.backgroundColor = "cyan";
+
+// const liByTag = document.querySelector("li"); 
+// console.log(discountNode);
+
+// const liById = document.querySelector("#discount");
+// console.log(liById);
+
+// const liByClass = document.querySelector(".yellow");
+// console.log(liByClass);
+
+// const lastItem = document.querySelector("li:last-child");
+// console.log(lastItem);
+
+// const firstItem = document.querySelector("li:first-child");
+// console.log(firstItem);
+
+// const discountedItems = document.querySelectorAll("#discount");
+// console.log(discountedItems);
+
+// console.log(discountedItems[0]);
+// console.log(discountedItems.length);
