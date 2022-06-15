a9808db8cdcc7101c01bc6999f4e556bcc4bad60
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-06-15 20:06:36 +0300

    Add todo POST

--- 25_paskaita/script.js
+++ 25_paskaita/script.js
@@ -1,58 +1,65 @@
 const renderTodo = (todo) => {
-    const { completed, title } = todo;
-  
-    const todoEl = document.createElement("div");
-    const todoTitle = document.createElement("p");
-    const todoStatus = document.createElement("span");
-  
-    todoEl.className = "todo";
-    todoTitle.className = "todo-title";
-    todoStatus.className = completed ? "todo-status done" : "todo-status";
-  
-    todoTitle.textContent = title;
-  
-    todoEl.append(todoTitle, todoStatus);
-    document.querySelector(".todo-container").prepend(todoEl);
+  const { completed, title } = todo;
+
+  const todoEl = document.createElement("div");
+  const todoTitle = document.createElement("p");
+  const todoStatus = document.createElement("span");
+
+  todoEl.className = "todo";
+  todoTitle.className = completed ? "todo-title done" : "todo-title";
+  todoStatus.className = completed ? "todo-status done" : "todo-status";
+
+  todoTitle.textContent = title;
+
+  todoStatus.addEventListener("click", () => {
+    // console.log(completed);
+    todoTitle.classList.toggle("done");
+    todoStatus.classList.toggle("done");
+    todoStatus.completed = !todoStatus.completed;
+  });
+
+  todoEl.append(todoTitle, todoStatus);
+  document.querySelector(".todo-container").prepend(todoEl);
+};
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((resp) => resp.json())
+  .then((response) => {
+    const todos = response.slice(0, 5);
+
+    todos.forEach((todo) => renderTodo(todo));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// issitraukti mygtuka +
+// priskirti addEvent listener +
+// issitraukti funkcijos viduje input lauka +
+// input duomenis append i todo lista
+
+const handleAddTodo = () => {
+  const todoText = document.querySelector('input[name="todo-text"]');
+  const newTodo = { title: todoText.value, completed: false };
+
+  const params = {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
   };
-  
-  fetch("https://jsonplaceholder.typicode.com/todos")
+
+  fetch("https://jsonplaceholder.typicode.com/todos", params)
     .then((resp) => resp.json())
     .then((response) => {
-      const todos = response.slice(0, 5);
-  
-      todos.forEach((todo) => renderTodo(todo));
+      renderTodo(response);
+      todoText.value = "";
     })
     .catch((error) => {
       console.error(error);
     });
-  
-  // issitraukti mygtuka +
-  // priskirti addEvent listener +
-  // issitraukti funkcijos viduje input lauka +
-  // input duomenis append i todo lista
-  
-  const handleAddTodo = () => {
-    const todoText = document.querySelector('input[name="todo-text"]');
-    const newTodo = { title: todoText.value, completed: false };
-  
-    const params = {
-      method: "POST",
-      body: JSON.stringify(newTodo),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    };
-  
-    fetch("https://jsonplaceholder.typicode.com/todos", params)
-      .then((resp) => resp.json())
-      .then((response) => {
-        renderTodo(response);
-        todoText.value = "";
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-  
-  const addButton = document.querySelector(".add-button");
-  addButton.addEventListener("click", handleAddTodo);
+};
+
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", handleAddTodo);
